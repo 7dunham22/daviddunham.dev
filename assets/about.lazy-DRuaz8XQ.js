e141515d9bc4ce0899710575245f937cd723c735
David Dunham <ddunham@swishdata.com>
2025-01-01 15:06:24 -0500

    Updates

---
+++ assets/about.lazy-DRuaz8XQ.js
@@ -0,0 +1,5 @@
+import{j as n,c as r}from"./index-J_49DJbu.js";function o(e){const t={a:"a",h2:"h2",p:"p",...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(t.h2,{children:"About"}),`
+`,n.jsxs(t.p,{children:[`Hi everyone. My name's David Dunham, and I'm currently an software developer\r
+with `,n.jsx(t.a,{href:"https://www.swishdata.com",children:"Swish Data"}),"."]}),`
+`,n.jsx(t.p,{children:`This blog site is currently a work in progress, and I look forward to sharing\r
+some of my thoughts and side projects with you soon.`})]})}function s(e={}){const{wrapper:t}=e.components||{};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}const c=r("/about")({component:a});function a(){return n.jsx(s,{})}export{c as Route};
