465a4a99a133f01bfd9b7fafd94f0356b30bc34f
David Dunham <daviddfh@gmail.com>
2025-02-02 10:12:43 -0500

    fix: correcting links in __root.tsx
    Signed-off-by: David Dunham <daviddfh@gmail.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,28 +8,31 @@ export const Route = createRootRoute({
     <>
       <h1>David Dunham</h1>
       <p>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
       </p>
       <p>
         Find me on &nbsp;
         <Link
           to="https://bsky.app/profile/daviddunham.dev"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           <img src={Bluesky} width={16} />
         </Link>
         , &nbsp;
         <Link
           to="https://www.linkedin.com/in/david-w-dunham/"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           <img src={Linkedin} width={16} />
         </Link>
         , and&nbsp;
         <Link
           to="https://github.com/7dunham22"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           <img src={Github} width={16} />
         </Link>
         .
